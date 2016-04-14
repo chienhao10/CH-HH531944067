03b476df4cf7a3ccbfa684a26d46a365701bc75e
chienhao10 <huangjianhao_10@hotmail.com>
2016-04-14 09:52:21 -0400

    放错文件

--- 基地大招/CH汉化-基地大招合作版/Main.cs
+++ 基地大招/CH汉化-基地大招合作版/Main.cs
@@ -17,34 +17,34 @@ namespace HumanziedBaseUlt
 
         public Main()
         {
-            Listing.config = MainMenu.AddMenu("HumanizedBaseUlts", "humanizedBaseUlts");
-            Listing.config.Add("on", new CheckBox("Enabled"));
-            Listing.config.Add("min20", new CheckBox("20 min passed"));
-            Listing.config.Add("minDelay", new Slider("Minimum ultimate delay", 1000, 0, 2500));
-            Listing.config.AddLabel("The time to let the enemy regenerate health in base");
-            Listing.config.Add("humanizedDelayOff", new CheckBox("TURN OFF HUMANIZED DELAY AT ALL", false));
+            Listing.config = MainMenu.AddMenu("CH汉化-基地大招", "humanizedBaseUlts");
+            Listing.config.Add("on", new CheckBox("开启"));
+            Listing.config.Add("min20", new CheckBox("经过了20分钟"));
+            Listing.config.Add("minDelay", new Slider("最低大招延迟", 1000, 0, 2500));
+            Listing.config.AddLabel("延迟时间等于敌人在基地回血的时间");
+            Listing.config.Add("humanizedDelayOff", new CheckBox("完全关闭人性化延迟", false));
 
             Listing.config.AddSeparator(20);
-            Listing.config.Add("fountainReg", new Slider("Fountain regeneration speed", 89, 50, 100));
-            Listing.config.Add("fountainRegMin20", new Slider("Fountain regeneration speed after minute 20", 331, 300, 350));
+            Listing.config.Add("fountainReg", new Slider("泉水回血速度", 89, 85, 92));
+            Listing.config.Add("fountainRegMin20", new Slider("20分钟后泉水回血速度", 364, 350, 370));
 
 
-            Listing.potionMenu = Listing.config.AddSubMenu("Potions");
-            Listing.potionMenu.AddLabel("[Regeneration Speed in HP/Sec.]");
-            Listing.potionMenu.Add("healPotionRegVal", new Slider("Heal Potion / Cookie", 11, 5, 20));
-            Listing.potionMenu.Add("crystalFlaskRegVal", new Slider("Crystal Flask", 10, 5, 20));
-            Listing.potionMenu.Add("crystalFlaskJungleRegVal", new Slider("Crystal Flask Jungle", 9, 5, 20));
-            Listing.potionMenu.Add("darkCrystalFlaskVal", new Slider("Dark Crystal Flask", 16, 5, 20));
+            Listing.potionMenu = Listing.config.AddSubMenu("药水");
+            Listing.potionMenu.AddLabel("[回血速度 HP/秒.]");
+            Listing.potionMenu.Add("healPotionRegVal", new Slider("药水 / 饼干", 10, 5, 20));
+            Listing.potionMenu.Add("crystalFlaskRegVal", new Slider("可充药剂", 10, 5, 20));
+            Listing.potionMenu.Add("crystalFlaskJungleRegVal", new Slider("猎人药水", 9, 5, 20));
+            Listing.potionMenu.Add("darkCrystalFlaskVal", new Slider("腐蚀药水", 16, 5, 20));
 
 
-            Listing.snipeMenu = Listing.config.AddSubMenu("Enemy Recall Snipe");
-            Listing.snipeMenu.AddLabel("[Premade feature added, too]");
+            Listing.snipeMenu = Listing.config.AddSubMenu("敌人回城时狙击");
+            Listing.snipeMenu.AddLabel("[联合技能已添加]");
 
-            Listing.snipeMenu.Add("snipeEnabled", new CheckBox("Enabled"));
-            AddStringList(Listing.snipeMenu, "minSnipeHitChance", "Minimum Snipe HitChance", 
-                new []{ "Impossible", "Low", "Above Average", "Very High"}, 2);
-            Listing.snipeMenu.Add("snipeDraw", new CheckBox("Draw Snipe paths"));
-            Listing.snipeMenu.Add("snipeCinemaMode", new CheckBox("Cinematic mode ™"));
+            Listing.snipeMenu.Add("snipeEnabled", new CheckBox("开启"));
+            AddStringList(Listing.snipeMenu, "minSnipeHitChance", "最低狙击命中率", 
+                new []{ "非常低", "低", "中高", "非常高"}, 2);
+            Listing.snipeMenu.Add("snipeDraw", new CheckBox("显示狙击路线"));
+            Listing.snipeMenu.Add("snipeCinemaMode", new CheckBox("影院观看模式 ™"));
 
             Listing.allyconfig = Listing.config.AddSubMenu("Premades");
             foreach (var ally in EntityManager.Heroes.Allies)
@@ -56,14 +56,14 @@ namespace HumanziedBaseUlt
             }
             
 
-            Listing.MiscMenu = Listing.config.AddSubMenu("Miscellaneous");
-            Listing.MiscMenu.AddLabel("[Draven]");
-            Listing.MiscMenu.Add("dravenCastBackBool", new CheckBox("Enable 'Draven Cast Back'"));
-            Listing.MiscMenu.Add("dravenCastBackDelay", new Slider("Cast Back X ms earlier", 400, 0, 500));
+            Listing.MiscMenu = Listing.config.AddSubMenu("杂项");
+            Listing.MiscMenu.AddLabel("[德莱文]");
+            Listing.MiscMenu.Add("dravenCastBackBool", new CheckBox("开启 '德莱文R2'"));
+            Listing.MiscMenu.Add("dravenCastBackDelay", new Slider("提早开启R2 X 毫秒", 400, 0, 500));
 
             Listing.MiscMenu.AddSeparator();
-            Listing.MiscMenu.Add("allyMessaging", new CheckBox("Send information to premades"));
-            Listing.MiscMenu.AddLabel("If only 1 person uses this addon the others get infromed via chat whisper");
+            Listing.MiscMenu.Add("allyMessaging", new CheckBox("给队友发送消息"));
+            Listing.MiscMenu.AddLabel("如果只有一名开黑队友使用这个脚本，将会发私信给其他路人队友");
 
             RecallTracker.RecallTracker.Init();
 
