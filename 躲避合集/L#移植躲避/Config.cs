3d6b215fbde569bfd22839490166800cf2fcb6eb
chienhao10 <huangjianhao_10@hotmail.com>
2016-05-13 17:59:00 -0400

    放回躲避合集

---
+++ 躲避合集/L#移植躲避/Config.cs
@@ -0,0 +1,222 @@
+// Copyright 2014 - 2014 Esk0r
+// Config.cs is part of Evade.
+// 
+// Evade is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Evade is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Evade. If not, see <http://www.gnu.org/licenses/>.
+
+#region
+
+using System;
+using System.Drawing;
+using System.Linq;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+
+#endregion
+
+namespace Evade
+{
+    internal static class Config
+    {
+        public const bool PrintSpellData = false;
+        public const bool TestOnAllies = false;
+        public static int SkillShotsExtraRadius
+        {
+            get { return 9 + humanizer["ExtraEvadeRange"].Cast<Slider>().CurrentValue; }
+        }
+        public static int SkillShotsExtraRange
+        {
+            get { return 20 + humanizer["ExtraEvadeRange"].Cast<Slider>().CurrentValue; }
+        }
+        public const int GridSize = 10;
+        public static int ExtraEvadeDistance
+        {
+            get { return 15 + humanizer["ExtraEvadeRange"].Cast<Slider>().CurrentValue; }
+        }
+
+        public static int ActivationDelay
+        {
+            get { return 0; /*return humanizer["ActivationDelay"].Cast<Slider>().CurrentValue;*/ }
+        }
+        public const int PathFindingDistance = 60;
+        public const int PathFindingDistance2 = 35;
+
+        public const int DiagonalEvadePointsCount = 7;
+        public const int DiagonalEvadePointsStep = 20;
+
+        public const int CrossingTimeOffset = 250;
+
+        public const int EvadingFirstTimeOffset = 250;
+        public const int EvadingSecondTimeOffset = 80;
+
+        public const int EvadingRouteChangeTimeOffset = 250;
+
+        public const int EvadePointChangeInterval = 300;
+        public static int LastEvadePointChangeT = 0;
+
+        public static Menu Menu, evadeSpells, skillShots, shielding, collision, drawings, misc, humanizer;
+        public static Color EnabledColor, DisabledColor, MissileColor;
+
+        public static void CreateMenu()
+        {
+            Menu = MainMenu.AddMenu("CH汉化-躲避", "evade");
+
+            if (Menu == null)
+            {
+                Chat.Print("LOAD FAILED", Color.Red);
+                Console.WriteLine("Evade:: LOAD FAILED");
+                throw new NullReferenceException("Menu NullReferenceException");
+            }
+
+            //Create the evade spells submenus.
+            evadeSpells = Menu.AddSubMenu("躲避技能", "evadeSpells");
+            foreach (var spell in EvadeSpellDatabase.Spells)
+            {
+                evadeSpells.AddGroupLabel(spell.Name);
+
+                try
+                {
+                    evadeSpells.Add("DangerLevel" + spell.Name, new Slider("危险等级", spell._dangerLevel, 1, 5));
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+
+                if (spell.IsTargetted && spell.ValidTargets.Contains(SpellValidTargets.AllyWards))
+                {
+                    evadeSpells.Add("WardJump" + spell.Name, new CheckBox("跳眼"));
+                }
+
+                evadeSpells.Add("Enabled" + spell.Name, new CheckBox("开启"));
+            }
+
+            //Create the skillshots submenus.
+            skillShots = Menu.AddSubMenu("敌方技能", "Skillshots");
+
+            foreach (var hero in ObjectManager.Get<AIHeroClient>())
+            {
+                if (hero.Team != ObjectManager.Player.Team || Config.TestOnAllies)
+                {
+                    foreach (var spell in SpellDatabase.Spells)
+                    {
+                        if (String.Equals(spell.ChampionName, hero.ChampionName, StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            skillShots.AddGroupLabel(spell.SpellName);
+                            skillShots.Add("DangerLevel" + spell.MenuItemName, new Slider("危险等级", spell.DangerValue, 1, 5));
+
+                            skillShots.Add("IsDangerous" + spell.MenuItemName, new CheckBox("为危险", spell.IsDangerous));
+
+                            skillShots.Add("Draw" + spell.MenuItemName, new CheckBox("显示线圈"));
+                            skillShots.Add("Enabled" + spell.MenuItemName, new CheckBox("开启", !spell.DisabledByDefault));
+                        }
+                    }
+                }
+            }
+
+            shielding = Menu.AddSubMenu("友军护盾", "Shielding");
+
+            foreach (var ally in ObjectManager.Get<AIHeroClient>())
+            {
+                if (ally.IsAlly && !ally.IsMe)
+                {
+                    shielding.Add("shield" + ally.ChampionName, new CheckBox("护盾 " + ally.ChampionName));
+                }
+            }
+
+            collision = Menu.AddSubMenu("体积碰撞", "Collision");
+            collision.Add("MinionCollision", new CheckBox("检查小兵体积碰撞"));
+            collision.Add("HeroCollision", new CheckBox("检查英雄体积碰撞"));
+            collision.Add("YasuoCollision", new CheckBox("检查亚索风墙阻挡"));
+            collision.Add("EnableCollision", new CheckBox("开启"));
+            //TODO add mode.
+
+            drawings = Menu.AddSubMenu("线圈", "Drawings");
+            //EnabledColor = drawings.AddColor(new[] { "ECA", "ECR", "ECG", "ECB"}, "Enabled Spell Color", Color.White);
+            //DisabledColor = drawings.AddColor(new[] {"DCA", "DCR", "DCG", "DCB"}, "Disabled Spell Color", Color.Red);
+            //MissileColor = drawings.AddColor(new[] { "MCA", "MCR", "MCG", "MCB" }, "Missile Color", Color.Red);
+
+            drawings.AddLabel("开启线圈颜色");
+            drawings.Add("EnabledDrawA", new Slider("A", 255, 1, 255));
+            drawings.Add("EnabledDrawR", new Slider("R", 255, 1, 255));
+            drawings.Add("EnabledDrawG", new Slider("G", 255, 1, 255));
+            drawings.Add("EnabledDrawB", new Slider("B", 255, 1, 255));
+
+            EnabledColor = Color.FromArgb(drawings["EnabledDrawA"].Cast<Slider>().CurrentValue,
+                drawings["EnabledDrawR"].Cast<Slider>().CurrentValue,
+                drawings["EnabledDrawG"].Cast<Slider>().CurrentValue,
+                drawings["EnabledDrawB"].Cast<Slider>().CurrentValue);
+
+            drawings.AddSeparator(5);
+
+            drawings.AddLabel("关闭线圈颜色");
+            drawings.Add("DisabledDrawA", new Slider("A", 255, 1, 255));
+            drawings.Add("DisabledDrawR", new Slider("R", 255, 1, 255));
+            drawings.Add("DisabledDrawG", new Slider("G", 255, 1, 255));
+            drawings.Add("DisabledDrawB", new Slider("B", 255, 1, 255));
+
+            DisabledColor = Color.FromArgb(drawings["DisabledDrawA"].Cast<Slider>().CurrentValue,
+                drawings["DisabledDrawR"].Cast<Slider>().CurrentValue,
+                drawings["DisabledDrawG"].Cast<Slider>().CurrentValue,
+                drawings["DisabledDrawB"].Cast<Slider>().CurrentValue);
+
+            drawings.AddSeparator(5);
+
+            drawings.AddLabel("弹道显示颜色");
+            drawings.Add("MissileDrawA", new Slider("A", 255, 1, 255));
+            drawings.Add("MissileDrawR", new Slider("R", 255, 1, 255));
+            drawings.Add("MissileDrawG", new Slider("G", 255, 1, 255));
+            drawings.Add("MissileDrawB", new Slider("B", 255, 1, 255));
+
+            MissileColor = Color.FromArgb(drawings["MissileDrawA"].Cast<Slider>().CurrentValue,
+                drawings["MissileDrawR"].Cast<Slider>().CurrentValue,
+                drawings["MissileDrawG"].Cast<Slider>().CurrentValue,
+                drawings["MissileDrawB"].Cast<Slider>().CurrentValue);
+
+            drawings.AddSeparator(5);
+
+            drawings.Add("EnabledDraw", new CheckBox("开启线圈"));
+            drawings.Add("DisabledDraw", new CheckBox("显示屏蔽的"));
+            drawings.Add("MissileDraw", new CheckBox("显示弹道"));
+
+            //drawings.AddItem(new MenuItem("EnabledColor", "Enabled spell color").SetValue(Color.White));
+            //drawings.AddItem(new MenuItem("DisabledColor", "Disabled spell color").SetValue(Color.Red));
+            //drawings.AddItem(new MenuItem("MissileColor", "Missile color").SetValue(Color.LimeGreen));
+            drawings.Add("Border", new Slider("宽度", 1, 5, 1));
+
+            drawings.Add("EnableDrawings", new CheckBox("开启线圈"));
+            drawings.Add("ShowEvadeStatus", new CheckBox("显示躲避状态"));
+
+            humanizer = Menu.AddSubMenu("人性化");
+
+            //humanizer.Add("ActivationDelay", new Slider("Activation Delay"));
+            humanizer.Add("ExtraEvadeRange", new Slider("额外躲避范围"));
+
+            misc = Menu.AddSubMenu("杂项", "Misc");
+            misc.AddStringList("BlockSpells", "躲避时屏蔽技能", new[] { "不", "危险时", "总是" }, 1);
+            //misc.Add("BlockSpells", "Block spells while evading").SetValue(new StringList(new []{"No", "Only dangerous", "Always"}, 1)));
+            misc.Add("DisableFow", new CheckBox("屏蔽躲避来自战争迷雾的技能"));
+            misc.Add("ShowEvadeStatus", new CheckBox("显示躲避状态"));
+            if (ObjectManager.Player.BaseSkinName == "Olaf")
+            {
+                misc.Add("DisableEvadeForOlafR", new CheckBox("奥拉夫开大时自动屏蔽躲避!"));
+            }
+
+            Menu.Add("Enabled", new KeyBind("开启按键", true, KeyBind.BindTypes.PressToggle, "K".ToCharArray()[0]));
+
+            Menu.Add("OnlyDangerous", new KeyBind("只躲避危险技能", false, KeyBind.BindTypes.HoldActive, 32)); //Space
+        }
+    }
+}
---
+++ 躲避合集/躲避#/EvadeMenu.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+
+namespace EvadePlus
+{
+    internal class EvadeMenu
+    {
+        public static Menu MainMenu { get; private set; }
+        public static Menu SkillshotMenu { get; private set; }
+        public static Menu SpellMenu { get; private set; }
+        public static Menu DrawMenu { get; private set; }
+        public static Menu HotkeysMenu { get; private set; }
+
+        public static readonly Dictionary<string, EvadeSkillshot> MenuSkillshots = new Dictionary<string, EvadeSkillshot>();
+
+        public static void CreateMenu()
+        {
+            if (MainMenu != null)
+            {
+                return;
+            }
+
+            MainMenu = EloBuddy.SDK.Menu.MainMenu.AddMenu("躲避#", "EvadePlus");
+
+            // Set up main menu
+            MainMenu.AddGroupLabel("一般设置");
+            MainMenu.Add("fowDetection", new CheckBox("开启战争迷雾探测"));
+            MainMenu.AddLabel("开启: 会躲避来自战争迷雾的技能, 关闭: 人性化一点，毕竟战争迷雾是黑的人反应没那么快");
+            MainMenu.AddSeparator(3);
+
+            MainMenu.Add("processSpellDetection", new CheckBox("开启技能生成探测"));
+            MainMenu.AddLabel("探测技能施放前就计算弹道 推荐开启");
+            MainMenu.AddSeparator(3);
+
+            MainMenu.Add("limitDetectionRange", new CheckBox("限制技能探测距离"));
+            MainMenu.AddLabel("只探测你附近的技能, 推荐开启");
+            MainMenu.AddSeparator(3);
+
+            MainMenu.Add("recalculatePosition", new CheckBox("允许重新计算躲避位置", false));
+            MainMenu.AddLabel("允许躲到一半找到新的躲避位置, 不推荐打开");
+            MainMenu.AddSeparator(3);
+
+            MainMenu.Add("moveToInitialPosition", new CheckBox("躲避后移动到之前希望到达的位置", false));
+            MainMenu.AddLabel("走到一半开始躲避，躲避后会走到刚才想去到的地方");
+            MainMenu.AddSeparator(3);
+
+            MainMenu.Add("serverTimeBuffer", new Slider("服务器时间缓冲", 0, 0, 200));
+            MainMenu.AddLabel("额外时间加入到躲避计算中");
+            MainMenu.AddSeparator();
+
+            MainMenu.AddGroupLabel("人性化");
+            MainMenu.Add("skillshotActivationDelay", new Slider("躲避延迟", 0, 0, 400));
+            MainMenu.AddSeparator(10);
+
+            MainMenu.Add("extraEvadeRange", new Slider("额外躲避距离", 0, 0, 300));
+            MainMenu.Add("randomizeExtraEvadeRange", new CheckBox("随机额外躲避距离", false));
+
+            // Set up skillshot menu
+            var heroes = Program.DeveloperMode ? EntityManager.Heroes.AllHeroes : EntityManager.Heroes.Enemies;
+            var heroNames = heroes.Select(obj => obj.ChampionName).ToArray();
+            var skillshots =
+                SkillshotDatabase.Database.Where(s => heroNames.Contains(s.SpellData.ChampionName)).ToList();
+            skillshots.AddRange(
+                SkillshotDatabase.Database.Where(
+                    s =>
+                        s.SpellData.ChampionName == "AllChampions" &&
+                        heroes.Any(obj => obj.Spellbook.Spells.Select(c => c.Name).Contains(s.SpellData.SpellName))));
+
+            SkillshotMenu = MainMenu.AddSubMenu("指向性技能");
+            SkillshotMenu.AddLabel(string.Format("Skillshots Loaded {0}", skillshots.Count));
+            SkillshotMenu.AddSeparator();
+
+            foreach (var c in skillshots)
+            {
+                var skillshotString = c.ToString().ToLower();
+
+                if (MenuSkillshots.ContainsKey(skillshotString))
+                    continue;
+
+                MenuSkillshots.Add(skillshotString, c);
+
+                SkillshotMenu.AddGroupLabel(c.DisplayText);
+                SkillshotMenu.Add(skillshotString + "/enable", new CheckBox("躲避"));
+                SkillshotMenu.Add(skillshotString + "/draw", new CheckBox("显示"));
+
+                var dangerous = new CheckBox("危险", c.SpellData.IsDangerous);
+                dangerous.OnValueChange += delegate(ValueBase<bool> sender, ValueBase<bool>.ValueChangeArgs args)
+                {
+                    GetSkillshot(sender.SerializationId).SpellData.IsDangerous = args.NewValue;
+                };
+                SkillshotMenu.Add(skillshotString + "/dangerous", dangerous);
+
+                var dangerValue = new Slider("危险数值", c.SpellData.DangerValue, 1, 5);
+                dangerValue.OnValueChange += delegate(ValueBase<int> sender, ValueBase<int>.ValueChangeArgs args)
+                {
+                    GetSkillshot(sender.SerializationId).SpellData.DangerValue = args.NewValue;
+                };
+                SkillshotMenu.Add(skillshotString + "/dangervalue", dangerValue);
+
+                SkillshotMenu.AddSeparator();
+            }
+
+            // Set up spell menu
+            SpellMenu = MainMenu.AddSubMenu("敌方技能");
+            SpellMenu.AddGroupLabel("闪现");
+            SpellMenu.Add("flash", new Slider("危险等级", 5, 0, 5));
+
+            // Set up draw menu
+            DrawMenu = MainMenu.AddSubMenu("线圈");
+            DrawMenu.AddGroupLabel("躲避线圈");
+            DrawMenu.Add("disableAllDrawings", new CheckBox("屏蔽所有线圈", false));
+            DrawMenu.Add("drawEvadePoint", new CheckBox("显示躲避位置"));
+            DrawMenu.Add("drawEvadeStatus", new CheckBox("显示躲避状态"));
+            DrawMenu.Add("drawDangerPolygon", new CheckBox("显示危险技能多边形线圈", false));
+            DrawMenu.AddSeparator();
+            DrawMenu.Add("drawPath", new CheckBox("显示自动躲避路径"));
+
+            // Set up controls menu
+            HotkeysMenu = MainMenu.AddSubMenu("快捷键");
+            HotkeysMenu.AddGroupLabel("开关");
+            HotkeysMenu.Add("enableEvade", new KeyBind("开启躲避", true, KeyBind.BindTypes.PressToggle, 'M'));
+            HotkeysMenu.Add("dodgeOnlyDangerous", new KeyBind("只躲避危险的", false, KeyBind.BindTypes.HoldActive));
+        }
+
+        private static EvadeSkillshot GetSkillshot(string s)
+        {
+            return MenuSkillshots[s.ToLower().Split('/')[0]];
+        }
+
+        public static bool IsSkillshotEnabled(EvadeSkillshot skillshot)
+        {
+            var valueBase = SkillshotMenu[skillshot + "/enable"];
+            return valueBase != null && valueBase.Cast<CheckBox>().CurrentValue;
+        }
+
+        public static bool IsSkillshotDrawingEnabled(EvadeSkillshot skillshot)
+        {
+            var valueBase = SkillshotMenu[skillshot + "/draw"];
+            return valueBase != null && valueBase.Cast<CheckBox>().CurrentValue;
+        }
+    }
+}
